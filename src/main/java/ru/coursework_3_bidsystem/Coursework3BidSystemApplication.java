15de60e845e111879904f070b6cb41703c004c59
Denis <protsdv@gmail.com>
2023-06-20 20:58:35 +0300

    Initial commit

---
+++ src/main/java/ru/coursework_3_bidsystem/Coursework3BidSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.coursework_3_bidsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework3BidSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursework3BidSystemApplication.class, args);
+    }
+
+}
